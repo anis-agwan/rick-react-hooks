4a539fd39775c33d26c8a85d755cc1e8b26b45d1
Mohmed Anis Agwan <anishagwan@gmail.com>
2020-08-12 00:22:06 +0530

    ..

--- src/pages/EpisodesPage/episodes-page.jsx
+++ src/pages/EpisodesPage/episodes-page.jsx
@@ -3,54 +3,29 @@ import axios from 'axios';
 import { Episodes } from '../../components/card-list/episodes.components';
 //import { Pagination } from '../../components/pagination.component';
 import { SearchBox } from "../../components/search-box/search.component";
-import { Pagination } from "../../components/pagination/pagination.component";
-
 
 const EpisodeList = () => {
-  const [episodes, setEpisodes] = useState([]);
-  const [info, setInfo] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage] = useState(1);
   const [searchField, setSearchField] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const [nextPage, setNextPage] = useState()
-  const [prevPage, setPrevPage] = useState()
-  const [epsPerPage] = useState(20);
-  const [totalPages, setTotal] = useState();
-  
 
-const handleChange = event => {
+  const handleChange = event => {
     setSearchField(event.target.value);
     console.log(searchField)
   };
-
   
-/*
-  useEffect(() => {
-    const fetchEpisodes = async () => {
-      setLoading(true);
-      const res = await axios.get(`https://rickandmortyapi.com/api/episode/?page=${currentPage}`);
-      setInfo(res.data.info);
-      setEpisodes(res.data.results);
-      setLoading(false);
-      setTotal(res.data.info.pages)
-    }
-
-    fetchEpisodes();
-  }, [currentPage]);
-*/
-
   useEffect(() => {
     const fetchSearch = async () => {
       setLoading(true);
-      const res = await axios.get(`https://rickandmortyapi.com/api/episode/?name=${searchField}`);
-      setInfo(res.data.info);
+      const res = await axios.get(`https://rickandmortyapi.com/api/episode/?name=${searchField}&page=${currentPage}`);
       setSearchResults(res.data.results)
       setLoading(false);
-      setTotal(res.data.info.pages)
     }
     fetchSearch();
-  }, [searchField]);
+  }, [searchField, currentPage]);
+
+  
 
   return (
     <div className="App">
@@ -59,7 +34,7 @@ const handleChange = event => {
         handleChange={handleChange} 
         value={searchField}/>
       <Episodes episodes={searchResults} loading={loading} />
-    </div>
+      </div>
   );
 }
 
